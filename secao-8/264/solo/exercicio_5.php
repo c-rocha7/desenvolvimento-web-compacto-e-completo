20ea1edf73968cc054b90655858efb8a67898649
Cauã R. Pereira <7aauac@gmail.com>
2024-02-28 14:38:06 -0300

    seção-8: aula 264 - exercício realizado sozinho

---
+++ secao-8/264/solo/exercicio_5.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo '<pre>';
+
+    /*
+        1. Apresentar o cálculo do perímetro de um quadrado com 3.2 metros de lado.
+
+        Resultado: 12.8 m
+    */
+
+    $lado = 3.2;
+    $perimetro = $lado * 4;
+    echo $perimetro;
