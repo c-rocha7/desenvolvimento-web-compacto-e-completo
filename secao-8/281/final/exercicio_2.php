8b0309552564ded75f65672b7aa595a69cc47b7f
Cauã R. Pereira <7aauac@gmail.com>
2024-02-28 16:58:28 -0300

    seção-8: aula 281 - exercício seguindo o professor

---
+++ secao-8/281/final/exercicio_2.php
@@ -0,0 +1,28 @@
+<?php
+
+    /* 
+    1. Constrói um array com todos os resultados da tabuada dos 327.
+    2. Apresenta os dados do array com um ciclo foreach (apenas os valores)
+    */
+
+    $resultados = [];
+    for ($i = 1; $i <= 10; $i++) {
+        $resultados[] = 327 * $i;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php foreach($resultados as $resultado): ?>
+        <h3><?= $resultado ?></h3>
+    <?php endforeach; ?>
+
+</body>
+</html>
