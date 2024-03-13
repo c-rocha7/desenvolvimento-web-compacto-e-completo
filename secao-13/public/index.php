b1a424fa1675aedfe9bd6000fbd08c069321cb9e
Cauã R. Pereira <7aauac@gmail.com>
2024-03-13 14:38:50 -0300

    seção-13: aula 438

--- secao-13/public/index.php
+++ secao-13/public/index.php
@@ -5,9 +5,3 @@ use bng\System\Router;
 require_once '../vendor/autoload.php';
 
 Router::dispatch();
-
-// $nomes = ['Leonard', 'Willie', 'Sue'];
-$nome = 'Mittie Fernandez';
-
-// printData($nomes);
-printData($nome);
