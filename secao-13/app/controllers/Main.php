a371dc1e0c378b24801c8795355038592af22549
Cauã R. Pereira <7aauac@gmail.com>
2024-03-13 21:28:35 -0300

    seção-13: aula 444

--- secao-13/app/controllers/Main.php
+++ secao-13/app/controllers/Main.php
@@ -4,6 +4,7 @@ namespace bng\Controllers;
 
 class Main extends BaseController
 {
+    // =======================================================
     public function index()
     {
         // check if there is no active user in session
@@ -43,6 +44,7 @@ class Main extends BaseController
         $this->view('layouts/html_footer');
     }
 
+    // =======================================================
     public function login_submit()
     {
         // check if there is already an active session
@@ -64,6 +66,25 @@ class Main extends BaseController
             $validation_errors[] = 'Username e password são obrigatórios.';
         }
 
+        // get form data
+        $username = $_POST['text_username'];
+        $password = $_POST['text_password'];
+
+        // check if username is valid email and between 5 and 50 chars
+        if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            $validation_errors[] = 'O username tem que ser um email válido.';
+        }
+
+        // check if username is between 5 and 50 chars
+        if (strlen($username) < 5 || strlen($username) < 50) {
+            $validation_errors[] = 'O username deve ter entre 5 e 50 caracteres.';
+        }
+
+        // check if password is valid
+        if (strlen($password) < 6 || strlen($password) > 12) {
+            $validation_errors[] = 'A password deve ter entre 6 e 12 caracteres.';
+        }
+
         // check if there are validation errors
         if (!empty($validation_errors)) {
             $_SESSION['validation_errors'] = $validation_errors;
@@ -71,10 +92,6 @@ class Main extends BaseController
 
             return;
         }
-
-        // get form data
-        $username = $_POST['text_username'];
-        $password = $_POST['text_password'];
 
         echo $username.'<br>'.$password;
     }
