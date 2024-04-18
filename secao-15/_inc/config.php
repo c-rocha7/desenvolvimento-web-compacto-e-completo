4de304600bcc714fa9074235e93356a79782ac30
Cauã R. Pereira <7aauac@gmail.com>
2024-04-18 11:42:53 -0300

    seção-15: aula 545

---
+++ secao-15/_inc/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('API_VERSION', '1.0.0');
+
+// api enabled or disabled
+define('API_ENABLED', true);
+define('API_MESSAGE', '');
+
+// mysql config
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DATABASE', 'udemy_clientes');
+define('MYSQL_USER', 'user_udemy_clientes');
+define('MYSQL_PASS', 'wic1pOg78OHOVO3Az5zu4E1eLIzEVI');
