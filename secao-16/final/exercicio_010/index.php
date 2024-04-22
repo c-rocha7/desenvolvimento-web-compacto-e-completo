4ca52e476d01567a0b136724e7dbc8e4028180aa
Cauã R. Pereira <7aauac@gmail.com>
2024-04-22 11:14:23 -0300

    seção-16: exercício seguindo o professor

---
+++ secao-16/final/exercicio_010/index.php
@@ -0,0 +1,23 @@
+<?php
+	$valores = [];
+	while (count($valores) < 20) {
+		$valor = rand(1, 100);
+		if (!in_array($valor, $valores)) {
+			$valores[] = $valor;
+		}
+	}
+	sort($valores);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php foreach ($valores as $valor) : ?>
+		<?= $valor ?><br>
+	<?php endforeach; ?>
+</body>
+</html>
