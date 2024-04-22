7e1ef0f8b68afca384bb5a82cc76e1b338d549f6
Cauã R. Pereira <7aauac@gmail.com>
2024-04-22 11:37:23 -0300

    seção-16: exercício seguindo o professor

---
+++ secao-16/final/exercicio_012/index.php
@@ -0,0 +1,24 @@
+<?php
+$pessoa = [
+	'nome'      => 'João',
+	'idade'     => 20,
+	'altura'    => 1.80,
+	'peso'      => 80,
+	'profissão' => 'Programador',
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+	<?php foreach($pessoa as $key => $value): ?>
+		<?= ucfirst($key) . ' = ' . $value?><br>
+	<?php endforeach; ?>
+
+</body>
+</html>
