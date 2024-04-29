9a6eb61acdcfd03742e5a504656fec9117f3dfa7
Cauã R. Pereira <7aauac@gmail.com>
2024-04-29 15:19:54 -0300

    seção-17: aula 582

---
+++ secao-17/582/app.js
@@ -0,0 +1,35 @@
+document.querySelector("#btn").addEventListener("click", () => {
+  const req = new XMLHttpRequest();
+
+  // síncrona
+  req.open(
+    "GET",
+    "http://localhost/secao-17/recursos_online/api/v1/get_all_users",
+    false
+  );
+  req.send();
+
+  // console.log(req.response);
+
+  // pegar os dados e passar para objeto de JS
+  if (req.status === 200) {
+    let dados = JSON.parse(req.response);
+    if (dados.message == "error") {
+      document.querySelector("#error").classList.remove("d-none");
+    } else {
+      let table_users = document.querySelector("#table_users");
+      table_users.innerHTML = null;
+      dados.results.forEach((user) => {
+        let tr = document.createElement("tr");
+        let html = `<td>${user.username}</td>`;
+        html += `<td>${user.email}</td>`;
+        html += `<td>${user.created_at}</td>`;
+        tr.innerHTML = html;
+        table_users.appendChild(tr);
+      });
+      document.querySelector("#tabela").classList.remove("d-none");
+    }
+  } else {
+    document.querySelector("#error").classList.remove("d-none");
+  }
+});
