0778fb71866484720d5f0384950765e1af89ed00
Cauã R. Pereira <7aauac@gmail.com>
2024-05-06 15:53:15 -0300

    seção-19: aula 654

---
+++ secao-19/654/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const server = express();
+server.listen(3000);
+
+// Rotas
+server.get('/', (req, res) => {
+    res.sendFile('./views/home.html', { root: __dirname });
+});
+
+server.get('/services', (req, res) => {
+    res.sendFile('./views/services.html', { root: __dirname });
+});
+
+server.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+
+server.get('/acerca_de', (req, res) => {
+    res.redirect('/about');
+});
+
+server.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
