2741f352f3c0538ca9c7ea8ef402aa361e446452
Cauã R. Pereira <7aauac@gmail.com>
2024-05-13 15:36:59 -0300

    ci_06

--- secao-20/app/Controllers/Main.php
+++ secao-20/app/Controllers/Main.php
@@ -3,12 +3,22 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UsuariosModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Main extends BaseController
 {
     public function index()
     {
-        echo 'Hello World!';
+        // echo 'Hello World!';
+
+        $model = new UsuariosModel();
+        $usuarios = $model->findAll();
+
+        // dd($usuarios);
+        // ou
+        echo '<pre>';
+        print_r($usuarios);
+        echo '</pre>';
     }
 }
