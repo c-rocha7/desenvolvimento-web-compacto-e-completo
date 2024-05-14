7b2142335fb37b93dcc0cba25a5388e348949f30
Cauã R. Pereira <7aauac@gmail.com>
2024-05-14 11:44:22 -0300

    ci_13

--- secao-20/app/Views/login_frm.php
+++ secao-20/app/Views/login_frm.php
@@ -14,9 +14,11 @@
 						<hr>
 						<div class="mb-3">
 							<input type="text" class="form-control" name="text_usuario" placeholder="Usuário" required>
+							<?php echo !empty($validation_errors['text_usuario']) ? '<p class="text-danger">'.$validation_errors['text_usuario'].'</p>' : ''; ?>
 						</div>
 						<div class="mb-3">
 							<input type="password" class="form-control" name="text_senha"  placeholder="Senha" required>
+							<?php echo !empty($validation_errors['text_senha']) ? '<p class="text-danger">'.$validation_errors['text_senha'].'</p>' : ''; ?>
 						</div>
 						<div class="mb-3">
 							<button class="btn btn-primary w-100">Entrar</button>
@@ -24,16 +26,6 @@
 
 					<?php echo form_close(); ?>
 
-					<?php if (!empty($validation_errors)) { ?>
-						<div class="alert alert-danger">
-							<ul>
-								<?php foreach ($validation_errors as $error) { ?>
-									<li><?php echo $error; ?></li>
-								<?php } ?>
-							</ul>
-						</div>
-					<?php } ?>
-					
 				</div>
 			</div>
 		</div>
