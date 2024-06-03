87f1cb979270c4bde27029f62badc556a2cf4d4d
Cauã R. Pereira <7aauac@gmail.com>
2024-06-03 18:15:29 -0300

    027

--- secao-21/app/Http/Controllers/MainController.php
+++ secao-21/app/Http/Controllers/MainController.php
@@ -150,8 +150,39 @@ class MainController extends Controller
     {
         $model = new TaskModel();
 
-        return $model->where('id_user_tasks', '=', session()->get('id'))
+        $tasks = $model->where('id_user_tasks', '=', session()->get('id'))
             ->whereNull('deleted_at')
             ->get();
+        $collection = [];
+        foreach ($tasks as $task) {
+
+            $link_edit = '<a href="'.route('edit_task', ['id' => $task->id]).'" class="btn btn-secondary m-1"><i class="bi bi-pencil-square"></i></a>';
+            $link_delete = '<a href="'.route('delete_task', ['id' => $task->id]).'" class="btn btn-secondary m-1"><i class="bi bi-trash"></i></a>';
+
+            $collection[] = [
+                'task_name' => $task->task_name,
+                'task_status' => $this->_status_name($task->task_status),
+                'task_actions' => $link_edit.$link_delete,
+            ];
+        }
+
+        return $collection;
+    }
+
+    private function _status_name($status)
+    {
+        $status_collection = [
+            'new' => 'Nova',
+            'in_progress' => 'Em Progresso',
+            'cancelled' => 'Cancelada',
+            'completed' => 'Concluída',
+        ];
+
+        if (array_key_exists($status, $status_collection)) {
+            return $status_collection[$status];
+        } else {
+            return 'Desconhecido';
+        }
+
     }
 }
--- secao-21/routes/web.php
+++ secao-21/routes/web.php
@@ -16,7 +16,15 @@ Route::middleware([CheckLogin::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('index');
     Route::get('/logout', [MainController::class, 'logout'])->name('logout');
 
-    // tasks
+    // tasks - new
     Route::get('/new_task', [MainController::class, 'new_task'])->name('new_task');
     Route::post('/new_task_submit', [MainController::class, 'new_task_submit'])->name('new_task_submit');
+
+    // tasks - edit
+    Route::get('/edit_task/{id}', [MainController::class, 'edit_task'])->name('edit_task');
+    Route::post('/edit_task_submit', [MainController::class, 'edit_task_submit'])->name('edit_task_submit');
+
+    // tasks - delete
+    Route::get('/delete_task/{id}', [MainController::class, 'delete_task'])->name('delete_task');
+    Route::post('/delete_task_confirm', [MainController::class, 'delete_task_confirm'])->name('delete_task_confirm');
 });
